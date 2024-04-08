6c99713d0af9c6e5209a5f5f158b4743f83feef4
Christopher Mancuyas <yashiro_08@yahoo.com>
2024-04-09 02:12:37 +0800

    Add project files.

---
+++ Intl.Realty.Firm/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Intl.Realty.Firm.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

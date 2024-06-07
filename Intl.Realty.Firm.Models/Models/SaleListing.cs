4421e27b54cb39c930caba3e3aedc007e749f11f
Christopher Mancuyas <yashiro_08@yahoo.com>
2024-06-07 19:10:33 +0800

    Sales-listing initial create

--- Intl.Realty.Firm.Models/Models/SaleListing.cs
+++ Intl.Realty.Firm.Models/Models/SaleListing.cs
@@ -10,5 +10,26 @@ namespace Intl.Realty.Firm.Models.Models
     {
         public int Id { get; set; }
         public string Description { get; set; } = string.Empty;
+        public string PropertyAddress { get; set; } = string.Empty;
+        public decimal FinalSalePrice { get; set; }
+        public DateTime FinalClosingDate { get; set; }
+        public decimal DepositAmount { get; set; }
+        public DateTime DepositDate { get; set;}
+        public string BuyerName { get; set; } = string.Empty;
+        public string LandLordName { get; set; } = string.Empty;
+        public Decimal ListingCommissionPercentage { get; set; }
+        public Decimal BuyingCommissionPercentage { get; set; }
+        public string ListingAgentName { get; set; } = string.Empty;
+        public string ListingBrokerage { get; set; } = string.Empty;
+        public string ListingBrokerageFax { get; set; } = string.Empty;
+        public string BuyerAgentName { get; set; } = string.Empty;
+        public string BuyerBrokerage { get; set; } = string.Empty;
+        public string BuyerBrokerageFax { get; set; } = string.Empty;
+        public string SellersLawyer { get; set; } = string.Empty;
+        public string SellersLawyerAddress { get; set; } = string.Empty;
+        public string SellersPhoneNumber { get; set; } = string.Empty;
+        public string BuyersLawyer { get; set; } = string.Empty;
+        public string BuyerssLawyerAddress { get; set; } = string.Empty;
+        public string BuyersPhoneNumber { get; set; } = string.Empty;
     }
 }

d3c22df4f40f976038416b9d7aca068600053f46
christopher.mancuyas <christopher.mancuyas@gmail.com>
2024-06-11 21:48:06 +0800

    crud-search-pagination-working

--- Intl.Realty.Firm/wwwroot/js/site.js
+++ Intl.Realty.Firm/wwwroot/js/site.js
@@ -2,3 +2,30 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+}
+
+function updateUrlParameter(param, value) {
+    var url = window.location.href;
+    var newUrl = url.replace(new RegExp('([?&])' + param + '=[^&#]*'), '$1' + param + '=' + value);
+    if (newUrl === url) {
+        newUrl = url + (url.indexOf('?') > -1 ? '&' : '?') + param + '=' + value;
+    }
+    window.history.replaceState({}, '', newUrl);
+}
+
+function handleToastrNotification(param, message) {
+    if (getUrlParameter(param) === 'True') {
+        toastr.success(message, '', {
+            timeOut: 10000,
+            closeButton: true,
+            onHidden: function () {
+                updateUrlParameter(param, '');
+            }
+        });
+    }
+}

a218296ce21c075505066d8da4714006a8cf9005
Christopher Mancuyas <yashiro_08@yahoo.com>
2024-07-24 22:44:27 +0800

    multiple-files upload already working

--- Intl.Realty.Firm/Program.cs
+++ Intl.Realty.Firm/Program.cs
@@ -6,6 +6,8 @@ using Intl.Realty.Firm.Service;
 using Intl.Realty.Firm.Service.IServices;
 using Intl.Realty.Firm.Models.Models;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -20,6 +22,32 @@ builder.Services.AddTransient<IConfigurationService, ConfigurationService>();
 builder.Services.AddTransient<IFileHandlerService, FileHandlerService>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+builder.Services.Configure<FormOptions>(o =>
+{
+    o.ValueLengthLimit = int.MaxValue;
+    o.MultipartBodyLengthLimit = int.MaxValue;
+    o.MultipartBoundaryLengthLimit = int.MaxValue;
+    o.MultipartHeadersCountLimit = int.MaxValue;
+    o.MultipartHeadersLengthLimit = int.MaxValue;
+    o.BufferBodyLengthLimit = int.MaxValue;
+    o.BufferBody = true;
+    o.ValueCountLimit = int.MaxValue;
+});
+
+builder.Services.Configure<IISServerOptions>(options =>
+{
+    options.MaxRequestBodySize = int.MaxValue;
+});
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.Limits.MaxRequestBodySize = int.MaxValue;
+});
+
+builder.WebHost.ConfigureKestrel(c =>
+{
+    c.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(100);
+});
 
 var app = builder.Build();
 

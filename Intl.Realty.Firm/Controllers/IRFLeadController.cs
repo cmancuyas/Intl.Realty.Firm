a72fd187188175a093043a7a1344c4651b2c9685
Christopher Mancuyas <yashiro_08@yahoo.com>
2024-08-21 00:32:38 +0800

    add controllers

---
+++ Intl.Realty.Firm/Controllers/IRFLeadController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Intl.Realty.Firm.Controllers
+{
+    public class IRFLeadController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Intl.Realty.Firm/Controllers/PurchaseIRFStocksController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Intl.Realty.Firm.Controllers
+{
+    public class PurchaseIRFStocksController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
